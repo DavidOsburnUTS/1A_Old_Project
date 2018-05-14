2e63cc4a97f2ff2815c625a6cacede0db3e1acd8
davidosburnUTS <david.osburn@student.uts.edu.au>
2018-05-14 20:48:42 +1000

    Added cardio training class

---
+++ Downloads/studio-group7-master/app/src/main/java/com/example/andre/ss1a_fitnessapp/CardioTraining.java
@@ -0,0 +1,24 @@
+package com.example.andre.ss1a_fitnessapp;
+
+/**
+ * Created by davidosburn on 14/5/18.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.ErrorReason;
+import com.google.android.youtube.player.YouTubePlayer.PlaybackEventListener;
+import com.google.android.youtube.player.YouTubePlayer.PlayerStateChangeListener;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+
+public class CardioTraining {
+}
