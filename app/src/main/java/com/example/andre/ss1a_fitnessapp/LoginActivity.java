275107a711b6f07e7ce7cf0cdca6b834cd592d04
Jacky <11708169@student.uts.edu.au>
2018-05-25 00:17:12 +1000

    typo in the code

--- app/src/main/java/com/example/andre/ss1a_fitnessapp/LoginActivity.java
+++ app/src/main/java/com/example/andre/ss1a_fitnessapp/LoginActivity.java
@@ -21,6 +21,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private EditText emailEditText;
     private EditText passwordEditText;
+    private boolean isFirstRun;
     //private TextView result;
     //private Button login;
 
@@ -37,6 +38,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
          findViewById(R.id.login_btn).setOnClickListener(this);
          findViewById(R.id.loginForgotPasswordBtn).setOnClickListener(this);
+
+        isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .getBoolean("isFirstRun", true);
 
 //=================================================================================================
 // Attempt to launch the register activity within the app
@@ -92,9 +96,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    Intent intent = new Intent(LoginActivity.this, GettingStartedActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent);
+                    if(isFirstRun) {
+
+                        getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit()
+                                .putBoolean("isFirstRun", false).commit();
+
+                        Intent intent = new Intent(LoginActivity.this, GettingStartedActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+
+                        finish();
+                    }
+                    else {
+                        Intent intent = new Intent(LoginActivity.this, HomepageActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
                     //finish();
                 }else{
                     Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
