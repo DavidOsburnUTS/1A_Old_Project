4587fbe49635e764889c3e5321ad44a343a1dffd
David Osburn <david.osburn@student.uts.edu.au>
2018-05-24 22:23:50 +1000

    added step counter

--- app/src/main/java/com/example/andre/ss1a_fitnessapp/StepListener.java
+++ app/src/main/java/com/example/andre/ss1a_fitnessapp/StepListener.java
@@ -4,5 +4,8 @@ package com.example.andre.ss1a_fitnessapp;
  * Created by davidosburn on 24/5/18.
  */
 
-public class StepListener {
+public interface StepListener {
+
+    public void step(long timeNs);
+
 }
