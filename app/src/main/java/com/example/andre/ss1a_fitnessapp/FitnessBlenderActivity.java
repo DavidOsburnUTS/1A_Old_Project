af7e3c45cee7ddcb6710c17d1c6f58ccb65abdc5
David Osburn <david.osburn@student.uts.edu.au>
2018-05-24 16:28:47 +1000

    minor changes

---
+++ app/src/main/java/com/example/andre/ss1a_fitnessapp/FitnessBlenderActivity.java
@@ -0,0 +1,40 @@
+package com.example.andre.ss1a_fitnessapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+import android.webkit.WebView;
+
+
+//Not sure if to include this or something similar, just further resources
+
+public class FitnessBlenderActivity extends AppCompatActivity {
+
+    public WebView wv;
+
+    public String URL = "https://www.fitnessblender.com/videos";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitness_blender);
+
+        wv = (WebView) findViewById(R.id.webView);
+        wv.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+
+                super.onPageFinished(view, url);
+                findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+
+            }
+        });
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl(URL);
+
+
+    }
+}
